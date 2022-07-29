842cbba94467e69f4ac11fdca9802c548a2ed0e1
Abhinav-Modi <abhinavmodi07@gmail.com>
2022-07-29 16:10:54 +0530

    fetch

--- index.js
+++ index.js
@@ -1,54 +1,39 @@
-require("dotenv").config();
-const express = require("express");
-const request = require('request');
+import express from "express";
+import fetch from "node-fetch";
 
-// const cors = require("cors");
 const app = express();
-app.get('/', (req, res) => {
-  res.send("GET Request Call0")
-})
 
+// import fetch from "node-fetch";
+// const cors = require("cors");
 
-
-
-
-app.post('/add', function(req, res){
-  console.log(req.body);
-  request.post(
-    {
-    url:'https://api.test.esamudaay.com/external/v1/ondc/retail/bpp/search',
-    
-    headers: {
-        'Content-Type': 'application/json'
-    }
-    },
-  function(error, response, body){
-    // console.log(error);
-    // console.log(response);
-    console.log(body);
-    res.send(body);
-  });
-  // res.send("body");
+app.get("/", (req, res) => {
+	res.send("success send request");
 });
-// app.use(
-//   cors({
-//     origin: true,
-//     credentials: true,
-//   })
-// );
+app.post("/add", async (req, res) => {
+	console.log("called post");
+	const url =
+		"https://api.test.esamudaay.com/external/v1/ondc/retail/bpp/search";
+	const options = {
+		method: "POST",
+		body: req.body,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	const response = await fetch(url, options)
+		.then((res) => res.json())
+		.catch((err) => {
+			console.error({
+				message: "error",
+				error: err,
+			});
+		});
+	console.log(response);
+	res.json(response);
+});
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (req, res) => {
-  console.log(`Server Started at PORT ${PORT}`);
+	console.log(`Server Started at PORT ${PORT}`);
 });
