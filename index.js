5ec372a6598cb8362803e5df2d840bff79e9a76b
Ayush-Batra <ayush.batra@learner.manipal.edu>
2022-07-29 13:57:09 +0530

    done

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 require("dotenv").config();
 const express = require("express");
+const request = require('request');
 
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
+app.get('/', (req, res) => {
+  res.send("GET Request Call0")
+})
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
+
+
+
+app.post('/add', function(req, res){
+  console.log(req.body);
+  request.post(
+    {
+    url:'https://api.test.esamudaay.com/external/v1/ondc/retail/bpp/search',
+    
+    headers: {
+        'Content-Type': 'application/json'
+    }
+    },
+  function(error, response, body){
+    // console.log(error);
+    // console.log(response);
+    console.log(body);
+    res.send(body);
+  });
+  // res.send("body");
 });
-const PORT = process.env.PORT || 4000;
+// app.use(
+//   cors({
+//     origin: true,
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (req, res) => {
   console.log(`Server Started at PORT ${PORT}`);
